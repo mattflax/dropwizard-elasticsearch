0f5730c4b62dc4a2afaf074d25ce6737f76a8875
matt <matt@flax.co.uk>
2018-04-05 16:59:28 +0100

    Implement cluster health check for Rest client.

--- src/main/java/io/dropwizard/elasticsearch/health/EsClusterHealthCheck.java
+++ src/main/java/io/dropwizard/elasticsearch/health/EsClusterHealthCheck.java
@@ -1,8 +1,15 @@
 package io.dropwizard.elasticsearch.health;
 
 import com.codahale.metrics.health.HealthCheck;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.cluster.health.ClusterHealthStatus;
+
+import javax.ws.rs.HttpMethod;
+import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -12,18 +19,22 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @see <a href="http://www.elasticsearch.org/guide/reference/api/admin-cluster-health/">Admin Cluster Health</a>
  */
 public class EsClusterHealthCheck extends HealthCheck {
+
+    static final String CLUSTER_HEALTH_ENDPOINT = "/_cluster/health";
+
     private final Client client;
+    private final RestHighLevelClient restClient;
     private final boolean failOnYellow;
 
     /**
-     * Construct a new Elasticsearch cluster health check.
+     * Construct a new Elasticsearch cluster health check using the
+     * TransportClient.
      *
      * @param client       an Elasticsearch {@link Client} instance connected to the cluster
      * @param failOnYellow whether the health check should fail if the cluster health state is yellow
      */
     public EsClusterHealthCheck(Client client, boolean failOnYellow) {
-        this.client = checkNotNull(client);
-        this.failOnYellow = failOnYellow;
+        this(checkNotNull(client), null, failOnYellow);
     }
 
     /**
@@ -37,6 +48,21 @@ public class EsClusterHealthCheck extends HealthCheck {
     }
 
     /**
+     * Construct a new Elasticsearch cluster health check using the RestClient.
+     * @param restClient a REST client configured for the cluster..
+     * @param failOnYellow whether the health check should fail if the cluster health state is yellow
+     */
+    public EsClusterHealthCheck(RestHighLevelClient restClient, boolean failOnYellow) {
+        this(null, checkNotNull(restClient), failOnYellow);
+    }
+
+    private EsClusterHealthCheck(Client client, RestHighLevelClient restClient, boolean failOnYellow) {
+        this.client = client;
+        this.restClient = restClient;
+        this.failOnYellow = failOnYellow;
+    }
+
+    /**
      * Perform a check of the Elasticsearch cluster health.
      *
      * @return if the Elasticsearch cluster is healthy, a healthy {@link com.codahale.metrics.health.HealthCheck.Result};
@@ -47,12 +73,39 @@ public class EsClusterHealthCheck extends HealthCheck {
      */
     @Override
     protected Result check() throws Exception {
-        final ClusterHealthStatus status = client.admin().cluster().prepareHealth().get().getStatus();
+        if (client != null) {
+            return checkTransportClient();
+        } else {
+            return checkRestClient();
+        }
+    }
 
+    private Result checkTransportClient() {
+        final ClusterHealthStatus status = client.admin().cluster().prepareHealth().get().getStatus();
+        return checkClusterHealthStatus(status);
+    }
+
+    private Result checkRestClient() throws Exception {
+        Response response = restClient.getLowLevelClient().performRequest(HttpMethod.GET, CLUSTER_HEALTH_ENDPOINT);
+        if (response.getStatusLine().getStatusCode() >= 300) {
+            return Result.unhealthy("Status error from server: %d - %s", response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
+        } else {
+            String body = EntityUtils.toString(response.getEntity());
+            Map jsonMap = new ObjectMapper().readValue(body, Map.class);
+            if (jsonMap.containsKey("status")) {
+                return checkClusterHealthStatus(ClusterHealthStatus.fromString(jsonMap.get("status").toString()));
+            } else {
+                return Result.unhealthy("No status in _cluster/health response - %s", body);
+            }
+        }
+    }
+
+    private Result checkClusterHealthStatus(ClusterHealthStatus status) {
         if (status == ClusterHealthStatus.RED || (failOnYellow && status == ClusterHealthStatus.YELLOW)) {
             return Result.unhealthy("Last status: %s", status.name());
         } else {
             return Result.healthy("Last status: %s", status.name());
         }
     }
+
 }
--- src/test/java/io/dropwizard/elasticsearch/health/EsClusterHealthCheckTest.java
+++ src/test/java/io/dropwizard/elasticsearch/health/EsClusterHealthCheckTest.java
@@ -1,21 +1,142 @@
 package io.dropwizard.elasticsearch.health;
 
+import com.codahale.metrics.health.HealthCheck;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.entity.ContentType;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.HttpMethod;
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for {@link EsClusterHealthCheck}
  */
 public class EsClusterHealthCheckTest {
+
+    private static final ProtocolVersion PROTOCOL_VERSION = new ProtocolVersion("HTTP", 1, 1);
+    private static final StatusLine BAD_STATUS_LINE = new BasicStatusLine(PROTOCOL_VERSION, 500, "Server error");
+    private static final StatusLine GOOD_STATUS_LINE = new BasicStatusLine(PROTOCOL_VERSION, 200, "OK");
+
+    private final RestHighLevelClient highLevelClient = mock(RestHighLevelClient.class);
+    private final RestClient lowLevelClient = mock(RestClient.class);
+
+    @Before
+    public void setup() {
+        when(highLevelClient.getLowLevelClient()).thenReturn(lowLevelClient);
+    }
+
     @Test(expected = NullPointerException.class)
-    public void initializationWithNullClientShouldFail() {
+    public void initializationWithNullTransportClientShouldFail() {
         new EsClusterHealthCheck(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void initializationWithNullRestClientShouldFail() {
+        new EsClusterHealthCheck((RestHighLevelClient) null, false);
     }
 
     @Test
     public void initializationWithClientShouldSucceed() {
         new EsClusterHealthCheck(mock(Client.class));
+    }
+
+    @Test(expected = IOException.class)
+    public void restClientThrowsIOException() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, true);
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenThrow(new IOException("Error"));
+
+        HealthCheck.Result result = healthCheck.check();
+        assertFalse(result.isHealthy());
+    }
+
+    @Test
+    public void restClientUnhealthyWhenStatusIsBad() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, true);
+        Response response = mock(Response.class);
+        when(response.getStatusLine()).thenReturn(BAD_STATUS_LINE);
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenReturn(response);
+
+        HealthCheck.Result result = healthCheck.check();
+        assertFalse(result.isHealthy());
+    }
+
+    @Test
+    public void restClientUnhealthyWhenStatusValueIsMissing() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, true);
+        Response response = mock(Response.class);
+        when(response.getStatusLine()).thenReturn(GOOD_STATUS_LINE);
+        when(response.getEntity()).thenReturn(new NStringEntity("{ }", ContentType.APPLICATION_JSON));
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenReturn(response);
+
+        HealthCheck.Result result = healthCheck.check();
+        assertFalse(result.isHealthy());
+    }
+
+    @Test
+    public void restClientUnhealthyWhenStatusIsRed() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, true);
+        Response response = mock(Response.class);
+        when(response.getStatusLine()).thenReturn(GOOD_STATUS_LINE);
+        when(response.getEntity()).thenReturn(new NStringEntity("{ \"status\": \"red\" }", ContentType.APPLICATION_JSON));
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenReturn(response);
+
+        HealthCheck.Result result = healthCheck.check();
+        assertFalse(result.isHealthy());
+    }
+
+    @Test
+    public void restClientUnhealthyWhenStatusIsYellow() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, true);
+        Response response = mock(Response.class);
+        when(response.getStatusLine()).thenReturn(GOOD_STATUS_LINE);
+        when(response.getEntity()).thenReturn(new NStringEntity("{ \"status\": \"yellow\" }", ContentType.APPLICATION_JSON));
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenReturn(response);
+
+        HealthCheck.Result result = healthCheck.check();
+        assertFalse(result.isHealthy());
+    }
+
+    @Test
+    public void restClientHealthyWhenStatusIsYellow() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, false);
+        Response response = mock(Response.class);
+        when(response.getStatusLine()).thenReturn(GOOD_STATUS_LINE);
+        when(response.getEntity()).thenReturn(new NStringEntity("{ \"status\": \"yellow\" }", ContentType.APPLICATION_JSON));
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenReturn(response);
+
+        HealthCheck.Result result = healthCheck.check();
+        assertTrue(result.isHealthy());
+    }
+
+    @Test
+    public void restClientHealthyWhenStatusIsGreen() throws Exception {
+        EsClusterHealthCheck healthCheck = new EsClusterHealthCheck(highLevelClient, false);
+        Response response = mock(Response.class);
+        when(response.getStatusLine()).thenReturn(GOOD_STATUS_LINE);
+        when(response.getEntity()).thenReturn(new NStringEntity("{ \"status\": \"green\" }", ContentType.APPLICATION_JSON));
+        when(lowLevelClient.performRequest(HttpMethod.GET, EsClusterHealthCheck.CLUSTER_HEALTH_ENDPOINT))
+                .thenReturn(response);
+
+        HealthCheck.Result result = healthCheck.check();
+        assertTrue(result.isHealthy());
     }
 }
